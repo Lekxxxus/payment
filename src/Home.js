a1417c71202e30259afb49c7ca2e1d794e050233
addtodo <ifortressalex@gmail.com>
2022-06-08 11:49:11 +0800

    first commit

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <di>
+        <div className="bg-red-200 w-32 h-32 text-blue-400">ALEX</div>
+      </di>
+    </div>
+  );
+}
+
+export default Home;
